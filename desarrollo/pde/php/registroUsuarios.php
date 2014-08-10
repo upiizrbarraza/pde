73b816bf981edd7b438966dfcbf0b213a4a492b4
BonjourUniverse <djcricker27@gmail.com>
2014-08-10 12:24:10 -0500

    Registro de Usuarios
    Registro de usuarios desde la página PED_DIAG_Login.php

---
+++ desarrollo/pde/php/registroUsuarios.php
@@ -0,0 +1,13 @@
+<?php
+	$u = $_GET['user'];
+	$email = $_GET['mail'];
+	$password = $_GET['pass'];
+
+	include 'conexion.php';
+
+	$pwd = md5($password);
+
+	$query="INSERT INTO usuarios(contrasena, correo, nombre, tipoUsuario, Departamentos_idDepartamento) values('".$pwd."', '".$email."', '".$u."',1,1)";
+	$mquery = mysql_query($query);
+	header('location: ../PED_DIAG_Login.php');
+?>
