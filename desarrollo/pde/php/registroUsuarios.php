7a1889a293c114c328ff90573213e24e0a748530
BonjourUniverse <djcricker27@gmail.com>
2014-08-13 13:46:36 -0500

    Registro de usuarios
    Se agregó un departamento para el público en general.

--- desarrollo/pde/php/registroUsuarios.php
+++ desarrollo/pde/php/registroUsuarios.php
@@ -7,7 +7,7 @@
 
 	$pwd = md5($password);
 
-	$query="INSERT INTO usuarios(contrasena, correo, nombre, tipoUsuario, Departamentos_idDepartamento) values('".$pwd."', '".$email."', '".$u."',1,1)";
+	$query="INSERT INTO usuarios(contrasena, correo, nombre, tipoUsuario, Departamentos_idDepartamento) values('".$pwd."', '".$email."', '".$u."',1,0)";
 	$mquery = mysql_query($query);
 	header('location: ../PED_DIAG_Login.php');
 ?>
