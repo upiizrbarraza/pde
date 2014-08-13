bea8ded763f4b46540efcd72c652361cb42c0b52
BonjourUniverse <djcricker27@gmail.com>
2014-08-13 14:09:41 -0500

    Inicio de sesión
    Se modificó el link al que redirecciona la página cuando hay una sesión
    activa

--- desarrollo/pde/php/login.php
+++ desarrollo/pde/php/login.php
@@ -2,7 +2,7 @@
       session_start();
 
       if(isset($_SESSION['userauth'])){
-        header('location: ../PED_DIAG_ApoyosMunicipalesIndex.php');
+        header('location: ../PED_DIAG_SDEIndex.php');
       }else{
 
       include 'conexion.php';
@@ -16,6 +16,7 @@
       $data=mysql_fetch_array($query);
       if ($row == 1){
       $_SESSION['userauth'] = $data['nombre'];
+      $_SESSION['iduser'] = $dara['idUsuario'];
       header('location: ../PED_DIAG_SDEIndex.php');
       }else{
       header('location: ../PED_DIAG_LoginPDE.php');
